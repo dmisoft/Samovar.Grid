d5ce47ccff30e00c6c63af28c228b66cd7d0a507
Dimitri Michel <post@dmisoft.de>
2023-11-16 00:05:36 +0100

    custom scroll thumb.init commit

---
+++ Samovar.Blazor.Server.Test/wwwroot/js/customScrollbar.js
@@ -0,0 +1,30 @@
+﻿// customScrollbar.js
+
+window.customScrollbar = {
+    handleMouseDown: function (dotNetObjectRef) {
+        document.addEventListener('mousemove', handleMouseMove);
+        document.addEventListener('mouseup', handleMouseUp);
+
+        function handleMouseMove(e) {
+            dotNetObjectRef.invokeMethodAsync('HandleMouseMove', e.clientY);
+            console.log(e.clientY);
+        }
+
+        function handleMouseUp() {
+            dotNetObjectRef.invokeMethodAsync('HandleMouseUp');
+            document.removeEventListener('mousemove', handleMouseMove);
+            document.removeEventListener('mouseup', handleMouseUp);
+        }
+    },
+
+    handleMouseMove: function (deltaY) {
+        var thumb = document.getElementById('customScrollbarThumb');
+        if (thumb && thumb.style) {
+            thumb.style.top = Math.max(0, thumb.offsetTop + deltaY) + 'px';
+        }
+    },
+
+    handleMouseUp: function () {
+        // Clean up any resources if needed
+    }
+};
