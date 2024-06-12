1fa0d99421e9930a080972f61444b184fb97a17d
Dimitri Michel <post@dmisoft.de>
2024-06-12 23:02:54 +0200

    aplha1

--- Samovar.Grid.Server.Test/Pages/Index.razor.cs
+++ Samovar.Grid.Server.Test/Pages/Index.razor.cs
@@ -1,109 +1,81 @@
 ﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using Samovar.Grid.Server.Test.Data;
 using System.ComponentModel.DataAnnotations;
 
-namespace Samovar.Grid.Server.Test.Pages
+namespace Samovar.Grid.Server.Test.Pages;
+public partial class Index
+    : ComponentBase
 {
-    public partial class Index
-        : ComponentBase
+    private List<WeatherForecast>? forecasts;
+    protected FormEditContext? EditContext;
+    SmGrid<WeatherForecast>? grid;
+
+    WeatherForecast? selectedRow = null;
+    uint pageSize = 10;
+    string gridWidth = "1200px";
+    string gridHeight = "600px";
+    string columnWidth = "150px";
+    string? cssClass;
+
+    RowSelectionMode selectionMode = RowSelectionMode.Single;
+    GridEditMode gridEditMode = GridEditMode.Form;
+    GridColumnResizeMode columnResizeMode = GridColumnResizeMode.Sliding;
+    IEnumerable<WeatherForecast> selectedRows = [];
+    bool showDetailRow = true;
+
+    async Task loadData()
     {
-        private List<WeatherForecast> forecasts;
-        FormEditContext EditContext = null;
-        SmGrid<WeatherForecast> grid;
+        forecasts = (await ForecastService.GetForecastAsync(DateTime.Now)).ToList();
+    }
 
-        WeatherForecast? selectedRow = null;
-        uint pageSize = 10;
-        string gridWidth = "1200px";
-        string gridHeight = "600px";
-        string columnWidth = "150px";
-        string? cssClass;
+    Task changeStyle()
+    {
+        cssClass = "blueTable";
+        return Task.CompletedTask;
+    }
 
-        RowSelectionMode selectionMode = RowSelectionMode.Single;
-        GridEditMode gridEditMode = GridEditMode.Form;
-        GridColumnResizeMode columnResizeMode = GridColumnResizeMode.Sliding;
-        IEnumerable<WeatherForecast> selectedRows = [];
-        bool showDetailRow = true;
+    Task RowEditBegin(WeatherForecast item)
+    {
+        return Task.CompletedTask;
+    }
 
-        async Task loadData()
+    Task RowInsertingHandler(WeatherForecast item)
+    {
+        forecasts ??= new List<WeatherForecast>();
+        forecasts = ((new WeatherForecast[] { item }).Concat(forecasts)).ToList();
+        return Task.CompletedTask;
+    }
+
+    Task RowRemovingHandler(WeatherForecast item)
+    {
+        forecasts = forecasts?.Except([item]).ToList();
+        return Task.CompletedTask;
+    }
+
+    Task deselectSingleSelectedItem()
+    {
+        selectedRow = null;
+        return Task.CompletedTask;
+    }
+
+    protected override Task OnInitializedAsync()
+    {
+        return base.OnInitializedAsync();
+    }
+
+    async Task collapseAllDetailRows() => await grid.CollapseAllDetailRows();
+
+    protected class FormEditContext
+    {
+        public FormEditContext(WeatherForecast dataItem)
         {
-            forecasts = (await ForecastService.GetForecastAsync(DateTime.Now)).ToList();
+            DataItem = dataItem;
         }
 
-        Task changeStyle()
-        {
-            cssClass = "minimalistBlack";
-            return Task.CompletedTask;
-        }
+        public WeatherForecast DataItem { get; set; }
 
-        Task RowEditBegin(WeatherForecast item)
-        {
-            return Task.CompletedTask;
-        }
-
-        void RowEditBeginHandler(WeatherForecast item)
-        {
-            EditContext = new FormEditContext(item);
-        }
-
-        Task<string> EditingFormTitle(WeatherForecast item)
-        {
-            return Task.FromResult(item.TemperatureC.ToString());
-        }
-
-        Task RowInsertingHandler(WeatherForecast item)
-        {
-            forecasts ??= new List<WeatherForecast>();
-            forecasts = ((new WeatherForecast[] { item }).Concat(forecasts)).ToList();
-            return Task.CompletedTask;
-        }
-
-        Task RowRemovingHandler(WeatherForecast item)
-        {
-            forecasts = forecasts.Except([item]).ToList();
-            return Task.CompletedTask;
-        }
-
-        Task deselectSingleSelectedItem()
-        {
-            selectedRow = null;
-            return Task.CompletedTask;
-        }
-
-        Task selectSingleSelectedItem()
-        {
-            selectedRow = forecasts.Skip(1).First();
-            return Task.CompletedTask;
-        }
-
-        protected override Task OnInitializedAsync()
-        {
-            return base.OnInitializedAsync();
-        }
-        async Task collapseAllDetailRows()
-        {
-            await grid.CollapseAllDetailRows();
-            //return Task.CompletedTask;
-        }
-
-        class FormEditContext
-        {
-            public FormEditContext(WeatherForecast dataItem)
-            {
-                DataItem = dataItem;
-                //Summary = DataItem.Summary;
-                //Region = DataItem.Region;
-                //City = DataItem.City;
-            }
-
-            public WeatherForecast DataItem { get; set; }
-
-            [Required]
-            [StringLength(maximumLength: 32, MinimumLength = 4, ErrorMessage = "The description should be 4 to 32 characters.")]
-            public string Summary { get; set; }
-
-            //public Action StateHasChanged { get; set; }
-        }
-
+        [Required]
+        [StringLength(maximumLength: 32, MinimumLength = 4, ErrorMessage = "The description should be 4 to 32 characters.")]
+        public string Summary { get; set; } = string.Empty;
     }
 }
--- Samovar.Grid/Docs/readme.md
+++ Samovar.Grid/Docs/readme.md
@@ -1,4 +1,7 @@
-﻿@page "/"
+﻿## Basic sample
+
+```csharp Index.razor
+@page "/"
 
 @using Samovar.Grid.Server.Test.Data
 @using System.ComponentModel.DataAnnotations
@@ -140,3 +143,90 @@
         }
     </div>
 </div>
+```
+
+```csharp Index.razor.cs
+using Microsoft.AspNetCore.Components;
+using Samovar.Grid.Server.Test.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace Samovar.Grid.Server.Test.Pages;
+public partial class Index
+    : ComponentBase
+{
+    private List<WeatherForecast>? forecasts;
+    protected FormEditContext? EditContext;
+    SmGrid<WeatherForecast>? grid;
+
+    WeatherForecast? selectedRow = null;
+    uint pageSize = 10;
+    string gridWidth = "1200px";
+    string gridHeight = "600px";
+    string columnWidth = "150px";
+    string? cssClass;
+
+    RowSelectionMode selectionMode = RowSelectionMode.Single;
+    GridEditMode gridEditMode = GridEditMode.Form;
+    GridColumnResizeMode columnResizeMode = GridColumnResizeMode.Sliding;
+    IEnumerable<WeatherForecast> selectedRows = [];
+    bool showDetailRow = true;
+
+    async Task loadData()
+    {
+        forecasts = (await ForecastService.GetForecastAsync(DateTime.Now)).ToList();
+    }
+
+    Task changeStyle()
+    {
+        cssClass = "blueTable";
+        return Task.CompletedTask;
+    }
+
+    Task RowEditBegin(WeatherForecast item)
+    {
+        return Task.CompletedTask;
+    }
+
+    Task RowInsertingHandler(WeatherForecast item)
+    {
+        forecasts ??= new List<WeatherForecast>();
+        forecasts = ((new WeatherForecast[] { item }).Concat(forecasts)).ToList();
+        return Task.CompletedTask;
+    }
+
+    Task RowRemovingHandler(WeatherForecast item)
+    {
+        forecasts = forecasts?.Except([item]).ToList();
+        return Task.CompletedTask;
+    }
+
+    Task deselectSingleSelectedItem()
+    {
+        selectedRow = null;
+        return Task.CompletedTask;
+    }
+
+    protected override Task OnInitializedAsync()
+    {
+        return base.OnInitializedAsync();
+    }
+
+    async Task collapseAllDetailRows() => await grid.CollapseAllDetailRows();
+
+    protected class FormEditContext
+    {
+        public FormEditContext(WeatherForecast dataItem)
+        {
+            DataItem = dataItem;
+        }
+
+        public WeatherForecast DataItem { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 32, MinimumLength = 4, ErrorMessage = "The description should be 4 to 32 characters.")]
+        public string Summary { get; set; } = string.Empty;
+    }
+}
+
+
+```
