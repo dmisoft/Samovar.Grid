9579bc7bbe8b5adb79c515027abf77450ef7e580
dmisoft <post@dmisoft.de>
2026-04-06 00:25:27 +0200

    test: refactor test project. Init commit

---
+++ Samovar.Grid.WasmTest/Data/WeatherForecast.cs
@@ -0,0 +1,37 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Samovar.Grid.WasmTest.Data;
+
+public class WeatherForecast
+{
+    [DisplayName("Pos.")]
+    public int Position { get; set; }
+    public DateTime Date { get; set; }
+
+    [Browsable(false)]
+    public DateOnly Date2 { get; set; }
+
+    [Display(Name ="Temp.C")]
+    public int? TemperatureC { get; set; }
+
+    [Browsable(false)]
+    public int? NullableInterger { get; set; } = null;
+
+    [Browsable(false)]
+    public int TemperatureF => 32 + (int)(TemperatureC ?? 0 / 0.5556);
+
+    [Browsable(false)]
+    public double TemperatureFD => 12.5;
+
+    public bool Boolean { get; set; }
+
+    public string Summary { get; set; } = string.Empty;
+
+    public WeatherForecastDetail Detail { get; set; } = new WeatherForecastDetail { SomeDetails = "Hello from details" };
+}
+
+public class WeatherForecastDetail
+{
+    public string SomeDetails { get; set; } = string.Empty;
+}
